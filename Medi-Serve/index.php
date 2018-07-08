e869b4bba9527c0316d050f91e2ebf7e634f57d2
Hemant Gangwar <34183672+spidohemant@users.noreply.github.com>
2018-07-08 09:43:57 +0530

    Add files via upload

---
+++ Medi-Serve/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Mediserve</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+
+<a href="login.php" class="container">>Log In</a><br>
+<a href="login.php" class="container">>Sign Up</a><br>
+<a href="donation form.php" class="container">>Donation Form</a><br>
+<a href="suggestionform.php" class="container">>Suggestion Form</a><br>
+<a href="contact form.php" class="container">>Contact Us Form</a><br>
+<a href="medicinebuying.php" class="container">>Medicine Buying Form</a><br>
+
+
+
+
+
+
+</div>
+
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
